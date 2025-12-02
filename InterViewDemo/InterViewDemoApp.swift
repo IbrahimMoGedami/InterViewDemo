9766ae37e0662cae00f2a177ca48045c0ff9bc38
Ibrahim Mohammed Gedami <hemagedamy@gmail.com>
2025-12-02 08:07:27 +0400

    Initial Commit

---
+++ InterViewDemo/InterViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  InterViewDemoApp.swift
+//  InterViewDemo
+//
+//  Created by Ibrahim Mo Gedami on 02/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct InterViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ InterViewDemoTests/InterViewDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  InterViewDemoTests.swift
+//  InterViewDemoTests
+//
+//  Created by Ibrahim Mo Gedami on 02/12/2025.
+//
+
+import Testing
+@testable import InterViewDemo
+
+struct InterViewDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
